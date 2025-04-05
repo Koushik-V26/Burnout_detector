fe5d17bba82c28c6f1f0ad71dcad028043df9b00
koushik <varanasikoushikkumar@gmail.com>
2025-04-05 22:10:53 +0530

    Data types , Variables , Operators

---
+++ Day1.js
@@ -0,0 +1,29 @@
+// Output Statemnts
+console.log('Hello,World!');
+
+// Data types
+let num = 10;
+let str = 'Hi';
+console.log(num,str)
+let a = '5';
+console.log(a+num);// On addition they performing explicit function
+console.log(num -a);//On subtraction they are performing implicit functions
+
+var b = 45;
+console.log(b);
+
+let name = 'VK';
+const age = 19;
+var city = 'Salur';
+console.log(name,age,city);
+let c = true; //Boolean datatype
+
+/* Operators */
+
+num1 = a+b;//arthimetic
+console.log(num);
+console.log(num1 < num);// Comaparision Operator
+console.log(5 == a);
+console.log(5 === a);//Strictly equality operator
+console.log(5!==a);//strictly inequality operator
+console.log(5===a || 5!==a);//Logical Operator
